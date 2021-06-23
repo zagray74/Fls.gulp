b8c3880cc6473ae7061bbf9bba64636ae6a62b64
zagray74git push -u origin maingit config --global user.name zagray74git config --global user.name zagray74 <v.zagray74@gmail.com>
2021-06-23 14:50:08 +0300

    finih

--- gulpfile.js
+++ gulpfile.js
@@ -36,9 +36,11 @@ let { src, dest } = require('gulp'),
     clean_css = require("gulp-clean-css"),
     rename = require("gulp-rename"),
     uglifly = require("gulp-uglify-es").default,
-    imagemin = require("gulp-imagemin");
+    imagemin = require("gulp-imagemin"),
+    ttf2woff = require("gulp-ttf2woff"),
+    ttf2woff2 = require("gulp-ttf2woff2");
 
-function browserSync(params) {
+function browserSync(params){
     browsersync.init({
         server: {
             baseDir: "./" + project_folder + "/"
@@ -96,11 +98,19 @@ function images() {
              progressive: true,
              svgoPlugins: [{ removeViewBox: false}],
              interlaced: true,
-             optimizationLevel: 3 ,
+             optimizationLevel: 3 
          })
      )
      .pipe(dest(path.build.img))
      .pipe(browsersync.stream())
+}
+function fonts() {
+    src(path.src.fonts)
+    .pipe(ttf2woff())
+    .pipe(dest(path.build.fonts));
+    return src(path.src.fonts)
+    .pipe(ttf2woff2())
+    .pipe(dest(path.build.fonts));
 }
 function watchFiles(params) {
     gulp.watch([path.watch.html],html);
@@ -112,9 +122,10 @@ function clean(params) {
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images,fonts));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
